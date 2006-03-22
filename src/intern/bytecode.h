cebda2d21d280f56ae681335751b8973ee53e323
thomj <thomj@9525544a-042d-0410-aeb9-addb4928da4d>
2006-03-22 22:42:25 +0000

    Made hello.c actualy test stuff
    git-svn-id: svn+ssh://mouse/var/svn/compsl/trunk@10 9525544a-042d-0410-aeb9-addb4928da4d

--- src/intern/bytecode.h
+++ src/intern/bytecode.h
@@ -8,16 +8,13 @@ typedef struct
   uint16_t code;
   union 
   {
-    union 
-    {
-       uint16_t a; // look up wether this works as expected
-       int16_t sa;
-    };
-    struct 
+    struct
     {
       uint8_t a1;
       uint8_t a2;
     };
+    uint16_t a; // look up wether this works as expected
+    int16_t sa;
   };
 } bytecode;
 #endif /*BYTECODE_H_*/
--- src/test/hello.c
+++ src/test/hello.c
@@ -1,11 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 #include "vm.h"
+#include "compart.h"
+#include "var.h"
+#include "intern/bytecode.h"
 #include "intern/vm.h"
+#include "compsl.h"
 
 int main()
 {
+	VM *vm = createVM();
+	compart *com = createComp();
 	
+	
+	//Tests
+	
+	
+	{// new block so code[] goes out of scope, this way we can re-use it's name in each test case
+		//test push/pop/add/end
+		bytecode code[] = 
+		{ 
+			{.code = 0x01, { {.a1 =0} } }, //push local at address 0
+			{.code = 0x01, { {.a1 =1} } }, //push local at address 1
+			{.code = 0x09}, // add
+			{.code = 0x29}  // end NOTE MAKE SURE THIS IS THE CORRECT VALUE MAY CHANGE!!!
+		};
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.i = 2;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.i = 3;
+		
+		runCubbyhole(com, 0);
+	}
+	//example case
+	{
+		bytecode code[] = 
+		{ 
+			{.code = 0x00, { {.a1 =0, .a2 = 0} }}, 
+			{.code = 0x00, {.a = 0} },
+			{.code = 0x00}
+		};
+	}
+	
+	//end tests
+	
+	destroyComp(com);
+	destroyVM(vm);
 	return 0; //return 0 else make's unhappy
 }

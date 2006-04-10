1aeb271ba5733fc0b77baf6554cc036222dc3e8a
thomj <thomj@9525544a-042d-0410-aeb9-addb4928da4d>
2006-04-10 17:51:51 +0000

    finished basic integer arithmatic test cases, added test for FADD, added first test case for jump instructions
    git-svn-id: svn+ssh://mouse/var/svn/compsl/trunk@64 9525544a-042d-0410-aeb9-addb4928da4d

--- src/test/test.c
+++ src/test/test.c
@@ -120,14 +120,166 @@ int main()
 			ret = 1;
 		}
 	}
+	
+	{ //MUL
+		bytecode code[] = 
+		{ 
+			{.code = BC_PUSH, { {.a1 =0} } }, //push local at address 0
+			{.code = BC_PUSH, { {.a1 =1} } }, //push local at address 1
+			{.code = BC_MUL}, // multiply
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_END}  // end NOTE MAKE SURE THIS IS THE CORRECT VALUE MAY CHANGE!!!
+		};
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.i = 2;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.i = 3;
+		
+		runCubbyhole(com, 0);
+		
+		if(com->vt.vars[0].v.i == 6)
+			printf("MUL: PASS!\n");
+		else { 
+			printf("MUL: FAIL!\n");
+			ret = 1;
+		}
+	}
+	
+	{ //DIV
+		bytecode code[] = 
+		{ 
+			{.code = BC_PUSH, { {.a1 =0} } }, //push local at address 0
+			{.code = BC_PUSH, { {.a1 =1} } }, //push local at address 1
+			{.code = BC_DIV}, // divide
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_END}  // end NOTE MAKE SURE THIS IS THE CORRECT VALUE MAY CHANGE!!!
+		};
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.i = 16;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.i = 2;
+		
+		runCubbyhole(com, 0);
+		
+		if(com->vt.vars[0].v.i == 8)
+			printf("DIV: PASS!\n");
+		else { 
+			printf("DIV: FAIL!\n");
+			ret = 1;
+		}
+	}
+	
+	{// FADD
+		bytecode code[] = 
+		{ 
+			{.code = BC_PUSH, { {.a1 =0} } }, //push local at address 0
+			{.code = BC_PUSH, { {.a1 =1} } }, //push local at address 1
+			{.code = BC_FADD}, // add
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_END} 
+		};
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.f = 2.0f;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.f = 0.5f;
+		
+		runCubbyhole(com, 0);
+		
+		if(com->vt.vars[0].v.f == 2.5f)
+			printf("FADD: PASS!\n");
+		else {
+			ret = 1;
+			printf("FADD: FAIL!\n");
+		}
+	}
+	
+	{ // JMP 1 
+		bytecode code[] = 
+		{ 
+			{.code = BC_PUSH, { {.a1 =1} } }, //push local at address 1
+			{.code = BC_PUSH, { {.a1 =2} } }, //push local at address 2
+			{.code = BC_JMP, {.a = 2} }, // jump forward, skip next instruction
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_END}
+		};
+		
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.i = 0;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.i = 1;
+		com->vt.vars[2].size =-1;
+		com->vt.vars[2].v.i = 2;
+		
+		runCubbyhole(com, 0);
+		
+		if(com->vt.vars[0].v.i == 2)
+			printf("JMP 1: PASS!\n");
+		else 
+		{
+			if(com->vt.vars[0].v.i == 1)
+				printf("JMP 1: FAIL! Didn't go far enough\n");
+			else
+				printf("JMP 1: FAIL! When't too far\n");
+			ret = 1;
+		}
+	}
+	
+	{ // JMP 2 (Backwards) this test is kinda dangerous, might infinite loop
+		bytecode code[] = 
+		{ 
+			{.code = BC_PUSH, { {.a1 =1} } }, //push local at address 1
+			{.code = BC_PUSH, { {.a1 =2} } }, //push local at address 2
+			{.code = BC_JMP, {.a = 4} }, // jump forward
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_POP, { {.a1 =0} }}, // pop into address 0
+			{.code = BC_END},
+			{.code = BC_JMP, {.sa = -2} }, // jump back 2
+			{.code = BC_END} // just to be safe....
+		};
+		
+		com->cubbys[0].code = code;
+		com->vm = vm;
+		
+		com->vt.vars[0].size =-1;
+		com->vt.vars[0].v.i = 0;
+		com->vt.vars[1].size =-1;
+		com->vt.vars[1].v.i = 1;
+		com->vt.vars[2].size =-1;
+		com->vt.vars[2].v.i = 2;
+		
+		runCubbyhole(com, 0);
+		
+		if(com->vt.vars[0].v.i == 2)
+			printf("JMP 2: PASS!\n");
+		else 
+		{
+			printf("JMP 2: FAIL!\n");
+			ret = 1;
+		}
+	}
+	
 	/*
 	//example case
 	{ // new block so code[] goes out of scope, this way we can re-use it's name in each test case
 		bytecode code[] = 
 		{ 
-			{.code = 0x00, { {.a1 =0, .a2 = 0} }}, 
-			{.code = 0x00, {.a = 0} },
-			{.code = 0x00}
+			{.code = BC_NOOP, { {.a1 =0, .a2 = 0} }}, 
+			{.code = BC_NOOP, {.a = 0} },
+			{.code = BC_NOOP},
+			{.code = BC_END}
 		};
 	}*/
 	

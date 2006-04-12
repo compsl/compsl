012e7d02f995c6328a88fd455a87178cb243e48b
thomj <thomj@9525544a-042d-0410-aeb9-addb4928da4d>
2006-04-12 15:37:28 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://mouse/var/svn/compsl/trunk@86 9525544a-042d-0410-aeb9-addb4928da4d

--- src/test/test-comp.c
+++ src/test/test-comp.c
@@ -6,11 +6,14 @@ int main()
 	printf("\nRunning compiler tests\n");
 	// fill this in
 	
-	char * files[] = {"src/test/base-structure.csl",
+	char * files[] = 
+	{
+		"src/test/base-structure.csl",
 		"src/test/casting.csl",
 		"src/test/declarations.csl",
 		"src/test/expressions.csl",
-		"src/test/flow-control.csl"};
+		"src/test/flow-control.csl"
+	};
 	  
 	  
 	for(int i=0;i<5;i++) {

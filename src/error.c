fbf309ed5213df3334dc8022651022494398da78
tomj <tomj@9525544a-042d-0410-aeb9-addb4928da4d>
2007-03-22 21:40:50 +0000

    added error strings for new error codes
    git-svn-id: svn+ssh://mouse/var/svn/compsl/trunk@227 9525544a-042d-0410-aeb9-addb4928da4d

--- src/error.c
+++ src/error.c
@@ -5,14 +5,14 @@ static const char *emessgs[] =
 {//TODO: fill in errors
 	"No Error",
 	"VARIABLE TABLE FULL: Cannot add variable",
+	"CONSTANT TABLE FULL: Cannot add constant",
 	"Cubbyhole Table full: Too many cubbyholes",
+	"Function Table full: Too many functions",
 	"No Such Cubbyhole: the specified name does not exist",
 	"No Such Variable: the specified name does not exist",
-	"",
-	"",
-	"",
-	"",
-	""
+	"No Such Function: the specified name does not exist",
+	"Cannot add function: Bad Function Parameter Format",
+	NULL
 };
 
 //static char *noSuchMessage = "Non-existant error message";
@@ -28,3 +28,4 @@ void compsl_printErr(COMPSL_ERROR err)
 	//TODO: find some way to bounds check this
 	fprintf(stderr, "ERROR %i: %s", err, emessgs[err]);
 }
+

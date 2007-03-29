5164625b647cc7a2e8a4a082d56e3c6f32bf93dd
tomj <tomj@9525544a-042d-0410-aeb9-addb4928da4d>
2007-03-29 15:45:44 +0000

    fixed compile errors
    git-svn-id: svn+ssh://mouse/var/svn/compsl/trunk@281 9525544a-042d-0410-aeb9-addb4928da4d

--- src/test/test-tourture.c
+++ src/test/test-tourture.c
@@ -11,15 +11,23 @@
 int main()
 {
 	VM *vm=createVM();
-	compart *com1=createComp();
-	compart *com2=createComp();
+	if(vm == NULL) { fprintf(stderr,"Error initializing compsl"); exit(1);}
+	
+	compart *com1=createComp(vm);
+	compart *com2=createComp(vm);
 	int compret1,compret2; // return code of compiler
-	if(vm == NULL || com1 == NULL || com2 == NULL)
+	if(com1 == NULL || com2 == NULL)
 	{
 		fprintf(stderr,"Error initializing compsl");
+		exit(1);
 	}
+	
+	//TODO: add variables to compartments here
 	
 	compret1 = fileCompile("../src/test/tourture.csl", vm, com1);
 	compret2 = fileCompile("../src/test/torture2.csl", vm, com1);
 	
+	//TODO: run init cubby on both in order here.
+	//TODO: check value of foo global in cubbys
+	//TODO: other tests
 }
